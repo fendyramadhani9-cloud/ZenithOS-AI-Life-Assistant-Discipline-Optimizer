c1a66dcc8408b0c4b2975da5670ba2fa2e7ec233
fendy <fendyramadhani9@gmail.com>
2026-08-19 23:09:43 +0700

    fix: upgrade to lucide_icons_flutter for Flutter 3.27 final IconData compatibility

--- lib/features/journal/widgets/dual_layer_journal_widget.dart
+++ lib/features/journal/widgets/dual_layer_journal_widget.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import '../../../core/constants/app_colors.dart';
 import '../../../core/constants/app_typography.dart';
 import '../../../core/storage/storage_service.dart';
